4ce818057bca9c812367cf44a86c0d95d1600820
laxon <br_lacson@yahoo.com>
2018-04-05 17:04:30 -0400

    basic programming

---
+++ 2TerminalHacker/Assets/Hacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hello Console");
+        //Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?\n");
+        Terminal.WriteLine("Press 1 for Facebook");
+        Terminal.WriteLine("Press 1 for Enron");
+        Terminal.WriteLine("Press 1 for Volkswagen\n");
+        Terminal.WriteLine("Enter your selection:");
+
+
+    }
+
+    void OnUserInput(string input) {
+        Terminal.WriteLine("You typed " + input);
+    }
+    
+}
