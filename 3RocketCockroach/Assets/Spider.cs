799ef928d01d1cec1d30025b63bce1df7218aeaf
laxon <br_lacson@yahoo.com>
2018-04-20 17:57:55 -0400

    added spider forward rotation

---
+++ 3RocketCockroach/Assets/Spider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print("Update");
+        ProcessInput();
+	}
+
+    private void ProcessInput() {
+
+        if (Input.GetKey(KeyCode.Space)) {
+            print("thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+       if (Input.GetKey(KeyCode.A)) {
+            print("rotating left");
+            transform.Rotate(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.D)) {
+            print("rotating right");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
